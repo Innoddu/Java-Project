09e9128b0262d9edbc2d51d713bd2562775863a6
Innoddu <ciui9494@gmail.com>
2020-12-07 22:17:05 +0900

    Projects

---
+++ ItemToPurchase.java
@@ -0,0 +1,83 @@
+/*
+ * 
+ * CS55 #1812 Inho Choi 1801787
+ * ItemPurchase list output Program Project
+ * 26 Nov 2020
+ * 
+ */
+public class ItemToPurchase {
+
+	// Private fields
+	private String item_name ="";
+	private String item_description="none";
+	private double item_price = 0;
+	private int itme_quantity = 0;
+	
+	public ItemToPurchase()
+	{
+		super();
+	}
+	/// Counstructor
+	public ItemToPurchase(String n, String d, double p, int q)
+	{
+	
+		this.item_name = n;
+		this.item_description = d;
+		this.item_price = p;
+		this.itme_quantity = q;
+	}
+	// mutator
+	public void setDescription(String d)
+	{
+		this.item_description=d;
+	}
+	// accessor
+	public String getDescription()
+	{
+		return item_description;
+	}
+	public String printItemCost()
+	{
+		// item name followed by quantatiy, price , subtotal
+		return item_name+" "+ itme_quantity + "@ $"+item_price+"= $"+(itme_quantity*item_price);
+		
+		
+		
+	}
+	public String printItemDescription()
+	{
+		// output item name and decription
+		return  item_name+":" +item_description;
+		
+	}
+	// mutator
+	public void setName(String n)
+	{
+		this.item_name=n;
+	}
+	// accessor
+	public String getName()
+	{
+		return item_name;
+	}
+	// mutator
+	public void setPrice(double p)
+	{
+		this.item_price=p;
+	}
+	// accessor
+	public double getPrice()
+	{
+		return item_price;
+	}
+	// mutator
+	public void setQuantity(int q)
+	{
+		this.itme_quantity=q;
+	}
+	// accessor
+	public int getQuantity()
+	{
+		return itme_quantity;
+	}
+}
---
+++ TestItemToPurchase.java
@@ -0,0 +1,154 @@
+/*
+ * 
+ * CS55 #1812 Inho Choi 1801787
+ * ItemPurchase list output Program Project
+ * 26 Nov 2020
+ * 
+ */
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+
+public class TestItemToPurchase {
+
+	public static void main(String[] args)   {
+		// TODO Auto-generated method stub
+		int input;
+		System.out.println("Welcome to JAVA MALL \n Press [1] Output ItemCost \n Press [2] Output Item Description \n Press [0] Quit\n");
+		Scanner sc = new Scanner(System.in);
+		input=sc.nextInt();
+
+		String n="";
+		String d="";
+		double p=0;
+		int q=0;
+		String an="";
+		
+		    // Create an object that converts characters into streams with System.in
+			InputStreamReader isr = new InputStreamReader(System.in);
+			// Create an object for isr
+			BufferedReader br = new BufferedReader(isr);
+			try {
+				// Create object to write on "report.txt"
+				FileWriter fw = new FileWriter("report.txt");
+				BufferedWriter bw= new BufferedWriter(fw);
+				
+		
+			while(input != -1) {
+					
+				// Output ItemCost 
+				
+				if(input ==1) {
+				System.out.println("Write Item Name: ");
+				 n = br.readLine();
+				System.out.println("Write Item Description: ");
+				d = br.readLine();	
+				System.out.println("Write Item Price: ");
+				p = Double.parseDouble(br.readLine());				
+				System.out.println("Write Item Quantity: ");
+				q = Integer.parseInt(br.readLine());
+				
+				ItemToPurchase itp = new ItemToPurchase(n,d,p,q);
+				
+				System.out.println("Are you add item more? Enter Y/N: ");
+				an = br.readLine();
+		
+				// Write on "report.txt"
+				bw.write(n);
+				bw.flush();
+				bw.newLine();
+				bw.write(d);
+				bw.flush();
+				bw.newLine();
+				bw.write(String.valueOf(p));
+				bw.flush();
+				bw.newLine();
+				bw.write(String.valueOf(q));
+				bw.flush();
+				bw.newLine();
+				bw.write(itp.printItemCost());
+				bw.flush();
+				bw.newLine();
+				System.out.println(itp.printItemCost());
+				if(an.equals("N") || an.equals("n"))
+				{
+				System.out.println("Welcome to JAVA MALL \n Press [1] Output ItemCost \n Press [2] Output Item Description \n Press [0] Quit\n");	
+				input=sc.nextInt();
+				}
+				
+
+				}
+				
+				// Output ItemDescription
+				else if(input ==2) 
+				{
+					System.out.println("Write Item Name: ");
+					 n = br.readLine();
+					System.out.println("Write Item Description: ");
+					d = br.readLine();	
+					System.out.println("Write Item Price: ");
+					p = Double.parseDouble(br.readLine());				
+					System.out.println("Write Item Quantity: ");
+					q = Integer.parseInt(br.readLine());
+					
+					ItemToPurchase itp = new ItemToPurchase(n,d,p,q);
+					
+					System.out.println("Are you add item more? Enter Y/N: ");
+					an = br.readLine();
+			
+					// Write on "report.txt"
+					bw.write(n);
+					bw.flush();
+					bw.newLine();
+					bw.write(d);
+					bw.flush();
+					bw.newLine();
+					bw.write(String.valueOf(p));
+					bw.flush();
+					bw.newLine();
+					bw.write(String.valueOf(q));
+					bw.flush();
+					bw.newLine();
+					bw.write(itp.printItemDescription());
+					bw.flush();
+					bw.newLine();
+					System.out.println(itp.printItemDescription());
+					if(an.equals("N") || an.equals("n"))
+					{
+						System.out.println("Welcome to JAVA MALL \n Press [1] Output ItemCost \n Press [2] Output Item Description \n Press [0] Quit\n");	
+						input=sc.nextInt();
+					}
+					}
+				  else if(input ==0)
+		    	  {
+		    		  //exit
+					 
+		    		  System.out.print("Check your \"report.txt\" file\nGood Bye :) \n");
+		    		 System.exit(0);
+		    		 
+		    	  }
+				}
+			
+				// Close BufferWriter
+				bw.close();
+				
+				
+			} catch (FileNotFoundException e)
+			{
+				e.printStackTrace();
+			}catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			
+	
+		
+	}
+
+
+}
